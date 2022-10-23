f542dfba51347b19b9e991d2904e38843248097e
Juan Cruz Alderete <juancruzalderete11@gmail.com>
2022-10-23 19:46:15 -0300

    Subida a git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoesListComponent } from './shoes-list/shoes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoesListComponent
   ],
   imports: [
     BrowserModule,
